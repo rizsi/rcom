8034b100bef486bc79c5ea990c78e6b3e5e7e534
rizsi <andras.schmidt.hu@gmail.com>
2020-04-22 01:01:17 +0200

    Enable PulseAudio echo cancel automatically before audio stream is started.

--- com.rizsi.rcom/src/com/rizsi/rcom/AbstractRcomArgs.java
+++ com.rizsi.rcom/src/com/rizsi/rcom/AbstractRcomArgs.java
@@ -16,6 +16,8 @@ public class AbstractRcomArgs {
 	public String program_v4l2="v4l2-ctl";
 	@JOHelp("ssh is started using this command (Used by secure connected client). It is possible to set full path if necessary.")
 	public String program_ssh="ssh";
+	@JOHelp("Disable using pactl to enable echo cancel. If not disabled then executes 'pactl list' to check if echo-cancel is activated and activates it if not: 'pactl load-module module-echo-cancel'")
+	public boolean disablePactlEchoCancel;
 	@JOHelp("The platform the program is running on. The parameters of the launched executables (ffmpeg webcam grabber, VNC) depend on the platform.")
 	public EPlatform platform;
 	@JOSimpleBoolean
--- com.rizsi.rcom/src/com/rizsi/rcom/audio/StreamSourceAudio.java
+++ com.rizsi.rcom/src/com/rizsi/rcom/audio/StreamSourceAudio.java
@@ -1,11 +1,15 @@
 package com.rizsi.rcom.audio;
 
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+
 import javax.sound.sampled.AudioFormat;
 
 import com.rizsi.rcom.IVideocomConnection;
 import com.rizsi.rcom.VideoConnection;
 import com.rizsi.rcom.cli.Client;
 
+import hu.qgears.commons.UtilProcess;
 import nio.multiplexer.OutputStreamSender;
 
 public class StreamSourceAudio implements AutoCloseable {
@@ -22,8 +26,49 @@ public class StreamSourceAudio implements AutoCloseable {
 	public static final int requestBufferSize=512;
 	private volatile ICapture capture;
 	private OutputStreamSender oss;
+	
+	private boolean isPulseAudioEchoCancelEnabled() throws IOException
+	{
+		ProcessBuilder pb=new ProcessBuilder("pactl", "list");
+		pb.redirectError(Redirect.INHERIT);
+		Process p=pb.start();
+		String res=UtilProcess.execute(p);
+		return res.indexOf("module-echo-cancel")>-1;
+	}
+	private void enablePulseAudioEchoCancel() throws IOException
+	{
+		System.out.println("pactl load-module module-echo-cancel");
+		ProcessBuilder pb=new ProcessBuilder("pactl", "load-module", "module-echo-cancel");
+		pb.redirectError(Redirect.INHERIT);
+		pb.redirectOutput(Redirect.INHERIT);
+		Process p=pb.start();
+		UtilProcess.execute(p);
+	}
 
 	public void start(Client client, String streamName) {
+		if(!client.getArgs().disablePactlEchoCancel)
+		{
+			try {
+				if(isPulseAudioEchoCancelEnabled())
+				{
+					System.out.println("PulseAudio Echo cancel is already enabled.");
+				}else
+				{
+					enablePulseAudioEchoCancel();
+					if(isPulseAudioEchoCancelEnabled())
+					{
+						System.out.println("PulseAudio Echo cancel enabled now.");
+					}else
+					{
+						System.err.println("Could not turn on PulseAudio echo cancel");
+					}
+				}
+
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		conn=client.conn;
 		oss=new OutputStreamSender(client.getMultiplexer(), VideoConnection.bufferSize, false);
 		client.conn.shareStream(oss.getId(), params=new StreamParametersAudio(streamName, client.id));
